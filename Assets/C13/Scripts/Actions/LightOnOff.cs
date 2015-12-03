8d402b7848e4a9479ba2478dcbdafcee4341792c
SUGINO Hironori <komenari@gmail.com>
2015-12-03 13:49:31 +0900

    シーン消したりした

---
+++ Assets/C13/Scripts/Actions/LightOnOff.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightOnOff : MonoBehaviour {
+    public KeyCode key;
+    public bool blink;
+
+    Light lit;
+	bool litDefault;
+    void Start () {
+        lit = GetComponent<Light>();
+        litDefault = lit.enabled;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if(blink){
+			if(Input.GetKey(key))
+				lit.enabled = !lit.enabled;
+			else
+				lit.enabled = litDefault;
+		}
+		else
+			if(Input.GetKeyDown(key))
+				lit.enabled = !lit.enabled;
+    }
+}
---
+++ Assets/C13/Scripts/Controller/RotateAround.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateAround : MonoBehaviour {
+    public Vector3 rotPos;
+    public Vector3 rotation;
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.RotateAround(rotPos, rotation.normalized, rotation.magnitude * Time.deltaTime);
+    }
+}
